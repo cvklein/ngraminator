7f769c9622ae1c535f989c5c909f9856270c757d
Colin Klein <cvklein@gmail.com>
2024-05-08 14:00:52 +1000

    bugfix

--- src/find_ngrams.py
+++ src/find_ngrams.py
@@ -97,7 +97,7 @@ def sequence_matcher_to_dataframe(a_lines,b_lines,a_map,b_map,cutoff=4): #return
     df['len'] = len_es
     return df
 
-def save_dataframe(df,filename,format='xlsx'):
+def save_dataframe(df,filename,format='excel'):
     if format == 'excel':
         df.to_excel(filename+'.xlsx',index=False)
     else:
