4371c3f7be6409fd7d5e3a02d7a1ddf447f6fdc5
Colin Klein <cvklein@gmail.com>
2024-05-08 14:21:36 +1000

    allow for printing if output file is not specified

--- src/ngraminator.py
+++ src/ngraminator.py
@@ -4,11 +4,11 @@ from find_ngrams import pull_lines,print_sequence_matches,sequence_matcher_to_da
 
 
 parser=argparse.ArgumentParser(prog='ngraminator',
-                    description='Usage ngraminator first_files second_files output_file',
+                    description='Usage ngraminator first_files second_files output_file. If output_file is missing, print instead.',
                     epilog='')
 parser.add_argument("first_files")
 parser.add_argument("second_files")
-parser.add_argument("output_file")
+parser.add_argument("output_file",nargs='?',default=None)
 parser.add_argument('--cutoff',default=4,type=int,help="cutoff. Only matches >=cutoff will be found. Defaults to 4 characters")
 parser.add_argument('--filetype',default='excel',choices=["excel","csv"],help="output filetype. Options are excel or csv. Defaults to excel.")
 args=parser.parse_args()
@@ -25,4 +25,7 @@ a_lines,a_map = pull_lines(glob(first_files))
 b_lines,b_map = pull_lines(glob(second_files))
 
 df = sequence_matcher_to_dataframe(a_lines,b_lines,a_map,b_map,cutoff=args.cutoff)
-save_dataframe(df,args.output_file,format=args.filetype)
+if args.output_file == None:
+    print(df)
+else:
+    save_dataframe(df,args.output_file,format=args.filetype)
