5a591c23fe24775603bd14c4a8356a6279dec24e
Colin Klein <cvklein@gmail.com>
2024-05-08 13:52:37 +1000

    Adding test data, setting up command line parsing

---
+++ src/ngraminator.py
@@ -0,0 +1,16 @@
+import argparse
+from find_ngrams import pull_lines,print_sequence_matches,sequence_matcher_to_dataframe,save_dataframe
+
+
+parser=argparse.ArgumentParser(prog='ngraminator',
+                    description='Usage ngraminator firstfiles secondfiles outputfile',
+                    epilog='')
+args=parser.parse_args()
+parser.add_argument("first_files")
+parser.add_argument("second_files")
+parser.add_argument("output_file")
+args=parser.parse_args()
+
+print(args.first_files)
+print(arg.second_files)
+print(arg.output_file)
