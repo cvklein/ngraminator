c0f8cbc6f76cdf95f8a7705efaeb6b83e2abcc3c
Colin Klein <cvklein@gmail.com>
2024-05-09 11:02:37 +1000

    allow separators to be read from file
    also add separator files for testing

--- src/ngraminator.py
+++ src/ngraminator.py
@@ -3,16 +3,17 @@ from glob import glob
 from find_ngrams import pull_lines,print_sequence_matches,sequence_matcher_to_dataframe,save_dataframe
 from tabulate import tabulate
 
-separators = ['，','。','；','？','「','」','：','！','《','》','、','．']
+
 
 parser=argparse.ArgumentParser(prog='ngraminator',
-                    description='Usage ngraminator first_files second_files output_file. If output_file is missing, print instead.',
+                    description='Usage ngraminator first_files second_files output_file. If output_file is missing, print to terminal instead.',
                     epilog='')
 parser.add_argument("first_files")
 parser.add_argument("second_files")
 parser.add_argument("output_file",nargs='?',default=None)
 parser.add_argument('--cutoff',default=4,type=int,help="cutoff. Only matches >=cutoff will be found. Defaults to 4 characters")
 parser.add_argument('--filetype',default='excel',choices=["excel","csv"],help="output filetype. Options are excel or csv. Defaults to excel.")
+parser.add_argument('--separators',default=None,help="specify a file with a list of separators to override the default list. Should contain a single line, and each character on that line will be used.")
 args=parser.parse_args()
 
 first_files = args.first_files
@@ -22,6 +23,14 @@ if first_files[-1] == "/":
     first_files += "*" #help glob work in an intuitive way
 if second_files[-1] == "/":
     second_files += "*"
+
+if args.separators == None:
+    separators = ['，','。','；','？','「','」','：','！','《','》','、','．']
+else:
+    f=open(args.separators,'r')
+    separators=list(f.readline())
+    f.close()
+
 
 a_lines,a_map = pull_lines(glob(first_files),separators=separators)
 b_lines,b_map = pull_lines(glob(second_files),separators=separators)
