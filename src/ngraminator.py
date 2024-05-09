25461a5ebc576ffa31c7534bf899e6397387f068
Colin Klein <cvklein@gmail.com>
2024-05-09 11:14:57 +1000

    cleanup help

--- src/ngraminator.py
+++ src/ngraminator.py
@@ -6,13 +6,13 @@ from tabulate import tabulate
 
 
 parser=argparse.ArgumentParser(prog='ngraminator',
-                    description='Usage ngraminator first_files second_files output_file. If output_file is missing, print to terminal instead.',
+                    description='If output_file is missing, print to terminal instead. Paths to directories will match all files in that directory.',
                     epilog='')
 parser.add_argument("first_files")
 parser.add_argument("second_files")
 parser.add_argument("output_file",nargs='?',default=None)
 parser.add_argument('--cutoff',default=4,type=int,help="cutoff. Only matches >=cutoff will be found. Defaults to 4 characters")
-parser.add_argument('--filetype',default='excel',choices=["excel","csv"],help="output filetype. Options are excel or csv. Defaults to excel.")
+parser.add_argument('--filetype',default='excel',choices=["excel","csv"],help="output filetype. Defaults to excel.")
 parser.add_argument('--separators',default=None,help="specify a file with a list of separators to override the default list. Should contain a single line, and each character on that line will be used.")
 args=parser.parse_args()
 
