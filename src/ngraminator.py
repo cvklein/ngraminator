a2eb8699ce7a358cca2b19d44d307866998190db
Colin Klein <cvklein@gmail.com>
2024-05-23 10:55:32 +1000

    cleanup
    cleanup after refactor, removing commented lines

--- src/ngraminator.py
+++ src/ngraminator.py
@@ -1,8 +1,5 @@
 import argparse
-#from glob import glob
-#from find_ngrams import pull_lines,print_sequence_matches,sequence_matcher_to_dataframe,save_dataframe
 from find_ngrams import process_input
-#from tabulate import tabulate
 
 
 
@@ -33,13 +30,3 @@ else:
     f.close()
 
 process_input(first_files,second_files,args.output_file,args.cutoff,separators,args.filetype)
-
-#a_lines,a_map = pull_lines(glob(first_files),separators=separators)
-#b_lines,b_map = pull_lines(glob(second_files),separators=separators)
-
-#df = sequence_matcher_to_dataframe(a_lines,b_lines,a_map,b_map,cutoff=args.cutoff)
-#if args.output_file == None:
-
-#    print(tabulate(df, showindex=False, headers=df.columns)) #going back and forth on this, but if you add ,disable_numparse=True it'll left-justify the length
-#else:
-#    save_dataframe(df,args.output_file,format=args.filetype)
