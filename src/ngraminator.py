5842853021a83864002a3d48243289f799e857a4
cvklein <cvklein@gmail.com>
2026-09-13 12:54:36 -0400

    cleaned up parser
    added a bit of code to make the help text prettier without changing value names

--- src/ngraminator.py
+++ src/ngraminator.py
@@ -3,10 +3,17 @@ from find_ngrams import process_input
 
 default_separators = ['，','。','；','？','「','」','：','！','《','》','、','．']
 
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def __init__(self, prog):
+        # max_help_position controls the starting column of the description
+        # width controls the overall terminal wrap width
+        super().__init__(prog, max_help_position=40, width=100)
+
 
 parser=argparse.ArgumentParser(prog='ngraminator',
                     description='If output_file is missing, print to terminal instead. Paths to directories will match all files in that directory.',
-                    epilog='')
+                    epilog='',
+                    formatter_class=CustomHelpFormatter)#this plus above allows for better formatting of help options
 parser.add_argument("first_files",nargs='?',default=None)
 parser.add_argument("second_files",nargs='?',default=None)
 parser.add_argument("output_file",nargs='?',default=None)
@@ -15,6 +22,7 @@ parser.add_argument('--filetype',default='excel',choices=["excel","csv"],help="o
 parser.add_argument('--separators',default=None,help="specify a file with a list of separators to override the default list. Should contain a single line, and each character on that line will be used.")
 parser.add_argument('--print_separators',action='store_true', help="Print the default list of separators and quit.")
 parser.add_argument('--preserve_digit_lines', action='store_true',help="Set to keep lines which start with an Arabic numeral (otherwise they're dropped)")
+
 args=parser.parse_args()
 
 
